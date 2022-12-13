189271adb37ba387b66a333128ab2d981d6ef864
wonil <https://github.com/imwonil/llllll-dd.git>
2022-12-13 20:31:07 +0900

    폴도 구조 최적화

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 "use strict";
 const express = require("express");
 const app =express();
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 
 //앱 셋팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine","ejs");
 
 
 app.use("/",home);//use -> 미들 웨어를 등록해주는 메소드;
 
-module.exports =app;
+module.exports =app; 
