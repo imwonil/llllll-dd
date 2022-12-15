235e4ecbf82c76a86cd877a0c7b4165221f32849
wonil <https://github.com/imwonil/llllll-dd.git>
2022-12-15 20:16:40 +0900

    로그인을 이용한

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,8 +1,8 @@
     "use strict";  
    
-const id = document.querySelector("#id"),
-psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("button");
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psword");
+const loginBtn = document.querySelector("button");
 
 
 
@@ -13,5 +13,12 @@ const req ={
 id: id.value,
 psword: psword.value,
 };
-console.log(req);
+
+fetch("/login",{
+  method: "POST",
+  headers: {
+    "Content-Type" : "application/json",
+  },  
+  body: JSON.stringify(req),  
+});
 }
