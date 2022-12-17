a68a056d235ef0aa72b39ce5cb8cf63ee8b8c640
wonil <https://github.com/imwonil/llllll-dd.git>
2022-12-17 18:49:31 +0900

    백엔드에서 로그인 구

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,16 +9,19 @@ const loginBtn = document.querySelector("button");
 loginBtn.addEventListener("click", login);
 
 function login() {
-const req ={
+const req = {
 id: id.value,
 psword: psword.value,
 };
 
-fetch("/login",{
+fetch("/login", {
   method: "POST",
   headers: {
-    "Content-Type" : "application/json",
+    "Content-Type" : "application/json"
   },  
   body: JSON.stringify(req),  
-});
+})
+.then((res) => res.json())
+.then((res) => console.log(res));
 }
+    
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,17 +1,43 @@
 "use strict"; 
+const users={
+id: ["wonil", "emc2wonil", "김팀장"],
+psword: ["1234", "1234", "123456"],
 
-function home(req, res){
-    res.render("home/index");
-}
-
-const  login = (req, res)=> {
-    res.render("home/login"); 
 }; 
 
 
+const output ={
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}; 
+    
 
-module.exports  ={ 
+const process = {
+login: (req, res) =>{
+const id = req. body. id,
+ psword = req. body. psword;
+   
+ if (users.id.includes(id)) {
+const idx = users.id.indexOf(id);
+if(users.psword[idx] === psword) {
+    return res.json({
+        success: true,
+    });
+  }
 
-home,
-login,
-}
+ }
+return res.json({
+    success: false,
+    msg: "로그인에 실패하셨습니다.",
+});
+},
+};  
+
+module.exports = { 
+process, 
+output,
+}; 
