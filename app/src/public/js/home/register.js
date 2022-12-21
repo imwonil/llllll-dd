656ffb60fb537ec5704c39f33d1d30e4fe101a71
wonil <https://github.com/imwonil/llllll-dd.git>
2022-12-21 23:29:48 +0900

    회원 가입 요청

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,42 @@
+"use strict";  
+   
+const id = document.querySelector("#id");
+const name = document.querySelector("#name")
+const psword = document.querySelector("#psword");
+const confirmPsword = document.querySelector("#confirm-psword ");
+const registerBtn = document.querySelector("#button");
+
+
+
+registerBtn.addEventListener("click", register );
+
+function register () {
+const req = {
+id: id.value,
+name: name.value,
+psword: psword.value,
+confirmPsword: confirmPsword.value,
+
+};
+ 
+
+fetch("/register", {
+  method: "POST",
+  headers: {
+    "Content-Type" : "application/json"
+  },  
+  body: JSON.stringify(req),  
+})
+.then((res) => res.json())
+.then((res) => {          
+if(res.success){
+location.href = "/login"; 
+} else {
+alert(res.msg);
+}
+})
+.catch((err) => { 
+console.error("회원가입 중 에러 발생");
+});
+
+}   
